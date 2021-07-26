21a64bf254ad776b7fa38694f297626429eefde8
Stanley <mat230821@hotmail.com>
2021-07-26 21:44:43 +0800

    relocate dotenv

--- lib/twilioValidation.dart
+++ lib/twilioValidation.dart
@@ -1,19 +1,17 @@
 import 'package:dio/dio.dart';
-import 'package:flutter_dotenv/flutter_dotenv.dart';
 
-class TwilioValidation{
+class TwilioValidation {
   final Dio _dio = Dio();
   static const int _HTTP_OK = 200;
 
   Future<int> phoneNoValidation(String phoneNo, String flag) async {
-    dotenv.load(fileName: "assets/.env");
     _dio.options.baseUrl = dotenv.env['API_URL']!;
-    _dio.options.headers =  {'Authorization': dotenv.env['API_AUTH_HEADER']};
+    _dio.options.headers = {'Authorization': dotenv.env['API_AUTH_HEADER']};
     _dio.options.connectTimeout = 3000;
     _dio.options.sendTimeout = 2000;
     _dio.options.receiveTimeout = 2000;
-    Response? response ;
-    do{
+    Response? response;
+    do {
       try {
         String temp = '$phoneNo?CountryCode=$flag';
         response = await _dio.get(temp);
@@ -29,12 +27,10 @@ class TwilioValidation{
           return e.response!.statusCode!;
         }
       }
-    }
-    while(response.statusCode == null || response.statusCode != _HTTP_OK);
+    } while (response.statusCode == null || response.statusCode != _HTTP_OK);
     if (response.statusCode == _HTTP_OK) {
       return _HTTP_OK;
     }
     return _HTTP_OK;
   }
-
 }
